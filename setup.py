040285b55b8f6c892acc883e4c16bd9d8609d765
John Clary <john.clary@austintexas.gov>
2019-06-09 12:31:20 -0700

    bumpversion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="atd_logutil",
-    version="0.0.1",
+    version="0.0.2",
     author="City of Austin",
     author_email="transportation.data@austintexas.gov",
     description="Python utilities for common logging tasks.",
